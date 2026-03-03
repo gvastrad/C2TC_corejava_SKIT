f757fcda6783e8071262985d399d6327ee453c67
Sagar <Sagar@LAPTOP-R2HUB5EQ>
2026-03-03 16:01:12 +0530

    first commit

---
+++ Core_Java_SKIT/src/com/tnsif/basics/FirstProgram.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.tnsif.basics;
+
+/**
+ * @author Sagar
+ *
+ */
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
